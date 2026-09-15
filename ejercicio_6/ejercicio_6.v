1c7c38b9e92ef065a5b5bd94ce348f78b1a9bdd9
Santiago Palacio <santiago.palaciova@upb.edu.co>
2026-09-15 16:37:52 -0500

    se añade el ejercicio 6

---
+++ ejercicio_6/ejercicio_6.v
@@ -0,0 +1,10 @@
+//Descripción de flujo de datos de un sumador de 4 bits
+module ejercicio_6(iA, iB, iCin, oSUM, oCout);
+input [3:0] iA, iB;
+input iCin;
+output [3:0] oSUM;
+output oCout;
+assign {oCout , oSUM} = iA + iB + iCin;
+endmodule
+
+
